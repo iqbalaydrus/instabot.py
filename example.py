2a10fad3c94e265654e6cfa796d2b7b8f280b6cf
Muhammad Iqbal Alaydrus <iqbal@niometrics.com>
2017-09-21 16:53:33 +0700

    log to file as default

--- example.py
+++ example.py
@@ -32,7 +32,7 @@ bot = InstaBot(
     unfollow_per_day=300,
     unfollow_break_min=15,
     unfollow_break_max=30,
-    log_mod=0,
+    log_mod=1,
     proxy='',
     # List of list of words, each of which will be used to generate comment
     # For example: "This shot feels wow!"
